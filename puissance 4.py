aa01232933fab8f0fcf1f7b21bd15d0889ab208d
Louisgllc <louis.gaillac@gmail.com>
2025-03-13 00:03:14 +0100

    interface 2.0

--- puissance 4.py
+++ puissance 4.py
@@ -19,27 +19,44 @@ root.rowconfigure(4, weight=1)
 root.rowconfigure(5, weight=1)
 root.rowconfigure(6, weight=1)
 root.rowconfigure(7, weight=1)
+root.rowconfigure(8, weight=1)
+root.rowconfigure(9, weight=1)
+root.rowconfigure(10, weight=1)
+root.rowconfigure(11, weight=1)
+root.rowconfigure(12, weight=1)
+root.rowconfigure(13, weight=1)
+root.rowconfigure(14, weight=1)
+root.rowconfigure(15, weight=1)
+#----------------------------------------------#
+#-------creation des fonctions à appeler-------#
+def affichage(self):
+    aff=tk.Label(root, text="Appuie sur le boutton ci-dessus pour connaitre es règles !", font=("haelvetica", 15), fg="lightgreen" )
+    aff.grid(row=11, column=1)
+    return
 #----------------------------------------------#
 #--------creation des widgets textuels---------#
 M1=tk.Label(root, text="Bienvenue sur Puissance 4 !", fg="red",
-                  font="Broadway")
+                  font=("Broadway", 45))
 
-M1.grid(column=1, row=0)
+M1.grid(column=1, row=2)
 #----------------------------------------------#
 #--------creation des widget boutons-----------#
-B1=tk.Button(root, text="Normal game", font="Arial",
-                   bg="lightgrey", fg="grey", relief="raised", padx=5, pady=5)
-B2=tk.Button(root, text="Sandbox", font="Arial",
-                   bg="lightgrey", fg="grey", relief="raised", padx=5, pady=5)
-B3=tk.Button(root, text="Options", font="Arial",
-                   bg="lightgrey", fg="grey", relief="raised", padx=5, pady=5)
-B4=tk.Button(root, text="Rules", font="Arial",
-                   bg="lightgrey", fg="grey", relief="raised", padx=5, pady=5)
+B1=tk.Button(root, text="Normal game", font=('haelvetica', 20),
+                   bg="lightgrey", fg="grey", relief="ridge", padx=10, pady=5)
+B2=tk.Button(root, text="Sandbox", font=('haelvetica', 20),
+                   bg="lightgrey", fg="grey", relief="ridge", padx=37, pady=5)
+B3=tk.Button(root, text="Options", font=('haelvetica', 20),
+                   bg="lightgrey", fg="grey", relief="ridge", padx=42, pady=5)
+B4=tk.Button(root, text="Rules", font=('haelvetica', 20),
+                   bg="lightgrey", fg="grey", relief="ridge", padx=54, pady=5)
 
-B1.grid(column=1, row=2)
-B2.grid(column=1, row=3)
-B3.grid(column=1, row=4)
-B4.grid(column=1, row=5)
+
+B4.bind("<Button-3>", affichage)
+
+B1.grid(row=7, column=1)
+B2.grid(row=8, column=1)
+B3.grid(row=9, column=1)
+B4.grid(row=10, column=1)
 #----------------------------------------------#
 
 
