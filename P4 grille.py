50321731bb3a5eb2852a05131359c1cc622aa93a
Antonin Lechanteux <antonin.lchntx@gmail.com>
2025-03-29 20:01:34 +0100

    LA GRAVITE

--- P4 grille.py
+++ P4 grille.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import random as rd
 
+
 #-----------creation de la fenetre-------------#
 root=tk.Tk()
 root.geometry("720x480")
@@ -13,7 +14,12 @@ root.config(bg = "#3394ff")
 #-----------creation de la grille-------------#
 HEIGHT = 720 #Sera modifiable
 WIDTH = 1295 #Sera modifiable
-dim_grille = [6,7] #jeu de base donc non modifiable
+ligne = 6
+colonne = 7
+dim_grille = [ligne,colonne] #jeu de base donc non modifiable
+grille = []
+for i in range(ligne):
+    grille.append([0] * colonne)
 canva_jeu = tk.Canvas(root, height=HEIGHT, width=WIDTH,bg="#005bff", borderwidth=0)
 canva_jeu.pack(expand=True)
 rayon_trou = (min((HEIGHT//dim_grille[0]),(WIDTH//dim_grille[1])))//2.5
@@ -23,19 +29,21 @@ for i in range(dim_grille[1]):
 #----------------------------------------------#
 
 #-----------creation des jetons-------------#
-tour = "j"
-grille = [[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0],[0,0,0,0,0,0,0]]
-diff_milieux_y = [660,540,420,300,180,60]
+
+diff_milieux_y = [i*HEIGHT//(ligne*2) for i in range(1,ligne*2,2)]
+diff_milieux_y.reverse()
+diff_milieux_x = [i*WIDTH//(colonne) for i in range(0,colonne+1)]
 rayon_jeton = 1.8 * rayon_trou / 2.13
+
 
 
 def verif(): #fonction qui prend en variable les coordonnees du jeton juste placé
     global win
     win = False
-    ligne()
-    colonne()
-    diag_droit_gauche()
-    diag_gauche_droit()
+    ligne_fct()
+    colonne_fct()
+    diag_droit_gauche_fct()
+    diag_gauche_droit_fct()
     if win == True:
         if tour == "j":
             print("omgomgomg jaune a gagné")
@@ -43,7 +51,7 @@ def verif(): #fonction qui prend en variable les coordonnees du jeton juste plac
             print("omgomgomg rouge a gagné")
     return
     
-def ligne(): #Fonction qui verifie si 4 jetons sont alignés en ligne
+def ligne_fct(): #Fonction qui verifie si 4 jetons sont alignés en ligne
     global win
     for i in grille:
         for j in range(4):
@@ -51,7 +59,7 @@ def ligne(): #Fonction qui verifie si 4 jetons sont alignés en ligne
                 if (i[j] == i[j+1] == i[j+2] == i[j+3]):
                     win = True
                     
-def colonne(): #Fonction qui verifie si 4 jetons sont alignés en colonne
+def colonne_fct(): #Fonction qui verifie si 4 jetons sont alignés en colonne
     global win
     for i in range(7):
         for j in range(3):
@@ -59,7 +67,7 @@ def colonne(): #Fonction qui verifie si 4 jetons sont alignés en colonne
                 if (grille[j][i] == grille[j+1][i] == grille[j+2][i] == grille[j+3][i]):
                     win = True
                     
-def diag_gauche_droit(): #fonction qui verifie si 4 jetons sont alignés en diagonale de la gauche en haut vers la droite en bas
+def diag_gauche_droit_fct(): #fonction qui verifie si 4 jetons sont alignés en diagonale de la gauche en haut vers la droite en bas
     global win
     for i in range(5,2,-1):
         for j in range(4):
@@ -67,7 +75,7 @@ def diag_gauche_droit(): #fonction qui verifie si 4 jetons sont alignés en diag
                 if (grille[i][j] == grille[i-1][j+1] == grille[i-2][j+2] == grille[i-3][j+3]):
                     win = True
             
-def diag_droit_gauche(): #fonction qui verifie si 4 jetons sont alignés en diagonale de la droite en haut vers la gauche en bas
+def diag_droit_gauche_fct(): #fonction qui verifie si 4 jetons sont alignés en diagonale de la droite en haut vers la gauche en bas
     global win
     for i in range(6,2,-1):
         for j in range(5,2,-1):
@@ -76,113 +84,46 @@ def diag_droit_gauche(): #fonction qui verifie si 4 jetons sont alignés en diag
                     win = True
         
  
+tour = "j"
+couleur_jeton = "#ffd933" 
+contour = "#e7ba00" 
 
 def placer_jeton(event):
     global tour
     global grille
+    global couleur_jeton
+    global contour
     coords_trou = canva_jeu.coords(canva_jeu.find_closest(event.x, event.y))
     (milieu_x,milieu_y) = ((coords_trou[0]+coords_trou[2])//2, (coords_trou[1]+coords_trou[3])//2)
-    if 0 <= milieu_x < 185: #Cas de la premiere colonne
-        if all(i[0] == 0 for i in grille): #cas ou toutes les trous de la colonne sont nulles
-            grille[0][0] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][0] != 0:
-            print("La premiere colonne est pleine") #Afficher quelque part sur l ecran que la colonne est pleine
-            return
-        else:
-            for i in range(4,-1,-1): #on regarde du haut vers le bas
-                if grille[i][0] != 0: #Et des qu'un trou est plein
-                    grille[i+1][0] = tour #On remplit celui d'au dessus
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 185 <= milieu_x < 370: #Cas de la deuxieme colonne
-        if all(i[1] == 0 for i in grille): 
-            grille[0][1] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][1] != 0:
-            print("La deuxieme colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][1] != 0:
-                    grille[i+1][1] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 370 <= milieu_x < 555: #Cas de la troisieme colonne
-        if all(i[2] == 0 for i in grille): 
-            grille[0][2] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][2] != 0:
-            print("La troisieme colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][2] != 0:
-                    grille[i+1][2] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 555 <= milieu_x < 740: #Cas de la quatieme colonne
-        if all(i[3] == 0 for i in grille): 
-            grille[0][3] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][3] != 0:
-            print("La quatrieme colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][3] != 0:
-                    grille[i+1][3] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 740 <= milieu_x < 925: #Cas de la cinquieme colonne
-        if all(i[4] == 0 for i in grille): 
-            grille[0][4] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][4] != 0:
-            print("La cinquieme colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][4] != 0:
-                    grille[i+1][4] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 925 <= milieu_x < 1110: #Cas de la sizieme colonne
-        if all(i[5] == 0 for i in grille): 
-            grille[0][5] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][5] != 0:
-            print("La sizieme colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][5] != 0:
-                    grille[i+1][5] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    elif 1110 <= milieu_x <= 1295: #Cas de la septieme colonne
-        if all(i[6] == 0 for i in grille): 
-            grille[0][6] = tour
-            milieu_y = diff_milieux_y[0]
-        elif grille[5][6] != 0:
-            print("La derniere colonne est pleine") 
-            return
-        else:
-            for i in range(4,-1,-1):
-                if grille[i][6] != 0:
-                    grille[i+1][6] = tour
-                    milieu_y = diff_milieux_y[i+1]
-                    break
-    if tour == "j":
-        canva_jeu.create_oval((milieu_x-rayon_jeton,milieu_y-rayon_jeton), (milieu_x+rayon_jeton, milieu_y+rayon_jeton),  fill = "#ffd933",  outline = "#e7ba00", width = 0.25*rayon_jeton)
-        verif()
-        tour = "r"
-        return
-    if tour == "r":
-        canva_jeu.create_oval((milieu_x-rayon_jeton,milieu_y-rayon_jeton), (milieu_x+rayon_jeton, milieu_y+rayon_jeton),  fill = "#ff3b30",  outline = "#bb261f", width = 0.25*rayon_jeton)
-        verif()
-        tour = "j"
-        return
+    
+    for i in range(colonne):   #pour chaque colonne
+        if diff_milieux_x[i] <= milieu_x < diff_milieux_x[i+1]: #Si la coordonnée x se trouve dans la ieme colonne, on entre dans la boucle
+            if all(j[i] == 0 for j in grille): #cas ou toutes les trous de la colonne sont nulles
+                grille[0][i] = tour #on place la couleur en bas de la grille virtuelle
+                milieu_y = diff_milieux_y[0] #on place le jeton tout en bas dans le canva
+                break #break pour eviter de faire tourner la boucle inutilement 
+            elif grille[ligne-1][i] != 0: #On verifie si 
+                print(f"La {i+1} ème colonne est pleine") #Afficher quelque part sur l écran que la colonne est pleine
+                return
+            else:
+                for t in range(ligne-1,-1,-1): #on regarde du haut vers le bas
+                    if grille[t][i] != 0: #et des qu'un trou est plein
+                        grille[t+1][i] = tour #on remplit celui d'au dessus
+                        milieu_y = diff_milieux_y[t+1]
+                        break
+
+    def animer_jeton(i):        
+        jeton = canva_jeu.create_oval((milieu_x - rayon_jeton, i - rayon_jeton),(milieu_x + rayon_jeton, i + rayon_jeton),fill=couleur_jeton,outline=contour,width=0.25 * rayon_jeton)
+        if i <milieu_y:
+            root.after(20, lambda: canva_jeu.delete(jeton))  # supprime l'ancien cercle
+            root.after(20, lambda: animer_jeton(i + HEIGHT // ligne ))  # récursivité qui descend jusqu'à atteindre la limite²
+
+    animer_jeton(diff_milieux_y[-1])
+    tour = "r" if tour == "j" else "j"
+    couleur_jeton = "#ffd933" if tour == "j" else "#ff3b30"
+    contour = "#e7ba00" if tour == "j" else "#bb261f"
+
 root.bind("<Button-1>", placer_jeton)
 
 root.mainloop()
+
